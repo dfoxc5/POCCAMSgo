7a9709a6b4659b23053bae5c8fbb7fb50eefd412
dfoxc5 <dfox@capitatech.com>
2017-11-09 15:49:16 -0800

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { EstatesComponent } from './estates.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EstatesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
+
 export class AppModule { }
